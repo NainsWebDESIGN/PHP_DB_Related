9a0473625fd38c4b6609fd4711ef2578fa4ba0fa
Nains <jason20226@gmail.com>
2021-01-07 15:04:53 +0800

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = ['', ''];
+  constructor(private http: HttpClient) { }
+  submitExample(_Nick, _Gen) {
+    console.log(_Nick.value);
+    console.log(_Gen.value);
+    const data = JSON.stringify({ "nickname": _Nick.value, "gender": _Gen.value })
+    this.http.post('assets/test.php', data).subscribe(el => {
+      console.log(el);
+    })
+  }
+  ngOnInit() {
+
+  }
 }
