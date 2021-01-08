4c61330309e3e469dd373951f4d18558e7d78f81
Nains <jason20226@gmail.com>
2021-01-08 13:54:08 +0800

    2021-01-08-初階PHP_POST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,15 @@ import { HttpClient, } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  text = "";
+  data: any = [];
   constructor(private http: HttpClient) { }
   submitExample(_Nick, _Gen) {
-    console.log(_Nick.value);
-    console.log(_Gen.value);
     const data = new FormData();
     data.append('nickname', _Nick.value)
     data.append('gender', _Gen.value)
     this.http.post('assets/test.php', data).subscribe(el => {
-      Array(el).length == 1 ? this.text = el[0] : this.text = "nickname is " + el[0] + " , gender is " + el[1];
+      this.data = el;
+      console.log(this.data);
     })
   }
   ngOnInit() {
