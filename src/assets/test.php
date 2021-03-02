86193b9faa1a1362d854105c643d50ebbe61de8b
Nains <jason20226@gmail.com>
2021-03-02 10:31:15 +0800

    2021-03-02-php arraypush

--- src/assets/test.php
+++ src/assets/test.php
@@ -3,22 +3,18 @@ header('Content-Type: application/json; charset=UTF-8'); //設定資料類型為
 if ($_SERVER['REQUEST_METHOD'] == "POST") { //如果是 POST 請求
     @$nickname = $_POST["nickname"]; //取得 nickname POST 值
     @$gender = $_POST["gender"]; //取得 gender POST 值
+    $data = array();
     if ($nickname && $gender) { //如果 nickname 和 gender 都有填寫
         //回傳 nickname 和 gender json 資料
-        echo json_encode(array(
-            'nickname' => $nickname,
-            'gender' => $gender
-        ));
+        array_push($data, array( 'nickname' => $nickname, 'gender' => $gender ));
     } else {
         //回傳 errorMsg json 資料
-        echo json_encode(array(
-            'errorMsg' => '資料未輸入完全！'
-        ));
+        array_push($data, array( 'errorMsg' => '資料未輸入完全！' ));
     }
+    echo json_encode($data);
 } else {
     //回傳 errorMsg json 資料
-    echo json_encode(array(
-        'errorMsg' => '請求無效，只允許 POST 方式訪問！'
-    ));
+        $data = array( 'errorMsg' => '請求無效，只允許 POST 方式訪問！' );
+    echo json_encode($data);
 }
 ?>
